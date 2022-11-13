1e1f880de107dacc0a73680849cb06f91154a3eb
Miroslav <103466192+user-ShDVR@users.noreply.github.com>
2022-11-13 12:08:53 +0500

    Delete redux directory

--- redux/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore} from '@reduxjs/toolkit'
-import auth from './slice/AuthSlice'
-export const store = configureStore({
-  reducer: {
-    auth: auth
-  },
-  devTools: true
-  
-})
