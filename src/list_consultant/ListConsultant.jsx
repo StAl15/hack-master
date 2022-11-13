eb33f6af518a7c3c6d27898628854c84efc44400
StAl15 <stihinaleksander@yandex.ru>
2022-11-13 10:30:12 +0500

    upd

--- src/list_consultant/ListConsultant.jsx
+++ src/list_consultant/ListConsultant.jsx
@@ -4,33 +4,57 @@ import './ListConsultant.css'
 
 const ListConsultant = () => {
     let cards = [{
-        'name': 'Маслов А.И.',
-        'quantity_clients': 52,
-        'rating': 4.58,
-        'earning': '+20%',
-        'img_src': 'https://www.vhv.rs/dpng/d/551-5511364_circle-profile-man-hd-png-download.png',
+        name: 'Маслов А.И.',
+        quantity_clients: 52,
+        rating: 4.58,
+        earning: '+20%',
+        img_src: 'https://www.vhv.rs/dpng/d/551-5511364_circle-profile-man-hd-png-download.png',
     },
     {
-        'name': 'Кирин А.И.',
-        'quantity_clients': 52,
-        'rating': 4.58,
-        'earning': '+20%',
-        'img_src': 'https://pngset.com/images/circle-profile-picture-face-person-human-clothing-transparent-png-488441.png',
+        name: 'Кирин А.И.',
+        quantity_clients: 52,
+        rating: 4.58,
+        earning: '+20%',
+        img_src: 'https://pngset.com/images/circle-profile-picture-face-person-human-clothing-transparent-png-488441.png',
     },
     {
-        'name': 'Шнюков А.И.',
-        'quantity_clients': 52,
-        'rating': 4.58,
-        'earning': '+20%',
-        'img_src': 'https://avatars.mds.yandex.net/i?id=d36979301ebc80156f88a6e76602df2a-4580514-images-thumbs&ref=rim&n=33&w=300&h=300',
+        name: 'Шнюков А.И.',
+        quantity_clients: 52,
+        rating: 4.58,
+        earning: '+20%',
+        img_src: 'https://avatars.mds.yandex.net/i?id=d36979301ebc80156f88a6e76602df2a-4580514-images-thumbs&ref=rim&n=33&w=300&h=300',
     },
-    
 
-];
+
+    ];
     return (
         <div className='content'>
             <div className='list_consultants'>
-                {cards.length ? <ConsultantCard cards={cards} /> : <p>Увы, экспертов нет</p>}
+                {/* {cards.length ? <ConsultantCard cards={cards} /> : <p>Увы, экспертов нет</p>} */}
+
+                <div>
+                    <div className='content_card'>
+
+                        <div className="round_rating_">
+                            <h3>Индекс доверия</h3>
+                            <h1>A +</h1>
+                        </div>
+                        <div className="quantity_clients">
+                            <h3 className="label">Количество клиентов</h3>
+                            <h3 className="data">40</h3>
+                        </div>
+                        <div className="rating">
+                            <h3 className="label">Средняя оценка</h3>
+                            <h3 className="data">4.4</h3>
+                        </div>
+                        <div className="earning">
+                            <h3 className="label">Средняя доходность</h3>
+                            <h3 className="data">+20%</h3>
+                        </div>
+                    </div>
+
+
+                </div>
             </div>
         </div>
     );
